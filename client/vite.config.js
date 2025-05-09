99e8d92dc45efeb4e1520a8a61cea7b5e37c96b9
Fiona Spencer <fionagspencer@gmail.com>
2025-05-08 22:24:43 -0400

    update

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,14 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import path from 'path'
 
 export default defineConfig({
+  plugins: [react()],
   server: {
     proxy: {
-      '/api' : {
+      '/api': {
         target: 'http://localhost:3000',
         secure: false,
       },
     },
   },
-  plugins: [react()],
+  build: {
+    outDir: path.resolve(__dirname, '../api/public'), // build output goes to backend
+    emptyOutDir: true,
+  },
+  base: '/', // optional; only change if serving from a subpath
 })
